075ea2e6c4aa5d656a975a6ec0a49531fa65109a
Thee5176 <theerapong5176@gmail.com>
2025-10-09 04:13:45 +0900

    update design document

--- bash/test_db_connection.sh
+++ bash/test_db_connection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # run this at ./envs/dev : cd ./envs/dev/
-psql -h "$(terraform output -raw rds_instance_address)" -U db_master -d record -p 5432
+psql -h "$(terraform output -raw rds_endpoint)" -U db_master -d record -p 5432
